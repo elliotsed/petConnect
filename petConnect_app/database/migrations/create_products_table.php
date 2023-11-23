1c46ca6d24f389cb50816177648b3be767e9542c
elliot <elliot.sedogbo@epitech.eu>
2023-11-23 10:44:41 +0100

    creation of the laravel project + database migration

---
+++ petConnect_app/database/migrations/create_products_table.php
@@ -0,0 +1,33 @@
+<?php
+
+use Illuminate\Database\Migrations\Migration;
+use Illuminate\Database\Schema\Blueprint;
+use Illuminate\Support\Facades\Schema;
+
+return new class extends Migration
+{
+    /**
+     * Run the migrations.
+     */
+    public function up(): void
+    {
+        Schema::create('products', function (Blueprint $table) {
+            $table->id();
+            $table->text('caracteristic');
+            $table->string('age');
+            $table->string('gender');
+            $table->foreignId('race_id')->constrained('races');
+            $table->string('photo');
+            $table->foreignId('user_id')->constrained('users');
+            $table->timestamps();
+        });
+    }
+
+    /**
+     * Reverse the migrations.
+     */
+    public function down(): void
+    {
+        Schema::dropIfExists('products');
+    }
+};
