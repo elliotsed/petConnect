dfd31c3857357186b0b33142bdda85445e86ab85
elliot <elliot.sedogbo@epitech.eu>
2023-11-29 18:10:41 +0100

    started the blog page + backup

--- petConnect_app/resources/views/blog.blade.php
+++ petConnect_app/resources/views/blog.blade.php
@@ -1,5 +1,6 @@
 <!DOCTYPE html>
 <html lang="en">
+
 <head>
     <meta charset="UTF-8">
     <meta name="viewport" content="width=device-width, initial-scale=1.0">
@@ -7,8 +8,24 @@
     @vite(['resources/js/app.css', 'resources/js/app.js'])
     <title>PetConnect-Blog</title>
 </head>
+
 <body>
-    @include("partials.navbar")
-    @include("partials.footer")
+    <div class="container">
+        @include('partials.navbar')
+
+        <div class="row text-center">
+            <h4>The Blog</h4>
+            <h2>WRITINGS FROM OUR EXPERTS</h2>
+            <p>All you have to know about dogs and how to raise them is gathered here</p>
+        </div>
+        <div class="row">
+            <img src="{{asset('images/blog/karsten-winegeart-KIVQIZnDbWI-unsplash.jpg')}}" alt="">
+        </div>
+
+        @include('partials.footer')
+    </div>
+
+
 </body>
+
 </html>
